37defe3e3996fcff23a9b464c49667f0a6128ae4
Andre Krykhtine <andkryk@gmail.com>
2025-03-06 09:19:09 -0300

    adiciona componentes Button e LoadingSpinner com estilos; ajusta importação do botão no tema

--- src/Componets/themeTogglerButton/style.jsx
+++ src/Componets/themeTogglerButton/style.jsx
@@ -1,4 +1,4 @@
-import { Button } from "../Button/Button";
+import { Button } from "../UI/Button/Button";
 import styled from "styled-components";
 
 export const StyledThemeButton = styled(Button)`
